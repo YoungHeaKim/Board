1aaf4f9bab695d2bd659046b9630b4f12ccf1d6c
younghea <dydgo9507@gmail.com>
2018-04-25 20:02:26 +0900

    로그인부분 router를 작성

--- src/user/user.js
+++ src/user/user.js
@@ -9,4 +9,23 @@ const app = express();
 
 const router = express.Router();
 
+// login 창
+router.get('/login', (req, res) => {
+  res.render(path.join(__dirname, '../views/login/login.ejs'));
+})
+
+// 회원가입부분
+// router.get('/register', (req, res) => {
+//   res.render(path.join(__dirname, '../../views/LogIn/register.ejs'));
+// });
+
+// 로그인
+router.post('/cookie', cookie.cookie);
+
+// 로그아웃
+router.get('/logout', cookie.cookieRemove);
+
+// 회원가입
+// router.post('/register', register.signUp);
+
 module.exports = router;
