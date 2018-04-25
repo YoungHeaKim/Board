a4f0076621c6b971c0ad14ee2e612019b41f40a9
younghea <dydgo9507@gmail.com>
2018-04-25 20:00:13 +0900

    두번들어가있는 것을 하나 삭제

--- src/models/article.js
+++ src/models/article.js
@@ -1,5 +1,4 @@
 const timestamps = require('mongoose-timestamp');
-const timestamps = require('mongoose-timestamp');
 const paginate = require('mongoose-paginate');
 const Schema = require('mongoose').Schema;
 const articleJson = require('./json/article.json');
