421d9bff6743f899d4b28671b7a7cbeb19d0df08
younghea <dydgo9507@gmail.com>
2018-04-25 20:00:59 +0900

    쿼리 작성

--- src/Query/AuthQuery.js
+++ src/Query/AuthQuery.js
@@ -1,5 +1,22 @@
 const User = require('../models/user');
 
 module.exports = {
-  
+  checkIdExist(email) {
+    return User.findOne({
+      email: email
+    });
+  },
+  checkUsernameExist(username) {
+    return User.findOne({
+      username: username
+    });
+  },
+  createUser(data) {
+    return User.create({
+      email: data.email,
+      password: data.password,
+      name: data.name,
+      username: data.username
+    })
+  },
 }
