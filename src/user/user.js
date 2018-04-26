e6602ec1f3f550e20c71bb4e56e513cd2d583bbc
younghea <dydgo9507@gmail.com>
2018-04-26 11:07:26 +0900

    회원가입부분 완료

--- src/user/user.js
+++ src/user/user.js
@@ -15,9 +15,9 @@ router.get('/login', (req, res) => {
 })
 
 // 회원가입부분
-// router.get('/register', (req, res) => {
-//   res.render(path.join(__dirname, '../../views/LogIn/register.ejs'));
-// });
+router.get('/register', (req, res) => {
+  res.render(path.join(__dirname, '../../views/LogIn/register.ejs'));
+});
 
 // 로그인
 router.post('/cookie', cookie.cookie);
@@ -26,6 +26,6 @@ router.post('/cookie', cookie.cookie);
 router.get('/logout', cookie.cookieRemove);
 
 // 회원가입
-// router.post('/register', register.signUp);
+router.post('/register', register.singUp);
 
 module.exports = router;
