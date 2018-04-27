87cbce765f27e2f7f19d4b444ebd4263512a1cc7
younghea <dydgo9507@gmail.com>
2018-04-28 00:33:06 +0900

    게시글을 삭제하는 부분 작성

--- src/article/article.delete.controller.js
+++ src/article/article.delete.controller.js
@@ -1,6 +1,11 @@
 const query = require('../Query');
 const ms = require('../message');
 
-exports.delete = (req, res) => {
-
+exports.delete = async (req, res) => {
+  const article = await query.removeArticle({ _id: req.params._id })
+  console.log(article);
+  if (!article) {
+    return res.status(500).json({ error: "데이터베이스 연결상에 문제가 있습니다." });
+  }
+  res.status(204).redirect('/article/lists');
 };
