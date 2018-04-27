f4b39270645a9361f5eb0a3ae91369571615fe70
younghea <dydgo9507@gmail.com>
2018-04-27 16:59:12 +0900

    정리 및 필요한 부분 추가
    오류 -> isLogin에 로그인을 하면 로그인된 유저의 정보를 저장하여야 하는데 저장을 하지 않음

--- src/index.js
+++ src/index.js
@@ -1,38 +1,85 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
-
 const express = require('express');
-const mongoose = require('mongoose');
 const morgan = require('morgan');
-const app = express();
-const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
-const passport = require('passport');
 const cookieParser = require('cookie-parser');
-const server = http.Server(app);
-global.db = mongoose.createConnection(process.env.MONGO_URI);
+const http = require('http');
 
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// passport 관련
+const passport = require('passport');
+const session = require('express-session');
+
+// mongodb 접속
+const mongoose = require('mongoose');
+global.db = mongoose.createConnection(process.env.MONGO_URI);
+const jwt = require('jsonwebtoken');
+const server = http.Server(app);
+const query = require('./Query');
 // user 부분
 const User = require('./user/user');
 // 게시판 부분
 const Article = require('./article/article');
 
 // ejs 템플릿
+// 확장자가 ejs 로 끈나는 뷰 엔진을 추가한다.
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// express application
+// 미들웨어 셋팅
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(morgan('dev'));
-app.use('/user', User);
-app.use('/article', Article);
+
+// upload path
+app.use('/uploads', express.static('uploads'));
+
+// session 관련 셋팅
+var connectMongo = require('connect-mongo');
+var MongoStore = connectMongo(session);
+
+var sessionMiddleWare = session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 2000 * 60 * 60 //지속시간 2시간
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 14 * 24 * 60 * 60
+  })
+});
+app.use(sessionMiddleWare);
 
 // Passport
 app.use(passport.initialize());
 app.use(passport.session());
+
+// 로그인정보를 뷰에서만 변수로 셋팅
+app.use( async (req, res, next) => {
+  const token = req.cookies.auth;
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await query.checkUserBy_id(decoded.id);
+  app.locals.isLogin = req.isAuthenticated();
+  app.locals.userData = req.user;
+  console.log(app.locals.isLogin)
+  console.log(app.locals.userData)  
+  console.log(token);
+  next()
+})
+
+app.use('/user', User);
+app.use('/article', Article);
+// 기본페이지를 리스트페이지로 변환
+app.use('/', (req, res) => {
+  return res.redirect('/article/lists');
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Successfully connected to mongodb'))
