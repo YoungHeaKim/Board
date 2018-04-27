2b5fe442778db8a7ed4e6a1dc8729cfb6afdca88
younghea <dydgo9507@gmail.com>
2018-04-28 00:38:50 +0900

    회원가입부분 작성

--- src/user/user.register.controller.js
+++ src/user/user.register.controller.js
@@ -46,6 +46,36 @@ exports.singUp = async (req, res) => {
   }
 }
 
-exports.edit = (req, res) => {
-  
+exports.edit = async (req, res) => {
+  // 1. 로그인이 되어있는 정보 가져오기
+  const token = req.cookies.auth;
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await query.checkUserBy_id(decoded.id);
+  const Password = user.password;
+  const checkPassword = req.body.checkPassword;
+  // 2. 로그인이 되어있는 유저의 비밀번호와 입력한 비번이 일치하는지 확인
+  const check = (user && bcrypt.compareSync(checkPassword, Password)) ? (null, user) : (null, false);
+  // 3. 확인이 되면 유저 정보를 수정
+  if (!check) {
+    return res.status(400).json('확인 비밀번호가 맞지않습니다.')
+  } else {
+    // 4. 수정한 정보를 데이터베이스에 저장
+    const userInfo = {
+      email: user.email,
+      password: bcrypt.hashSync(req.body.newPassword, 10),
+      name: user.name,
+      nickname: req.body.newNickname
+    };
+
+    // 이메일이 하나만 있는지 확인하는 부분
+    const checkNickname = await query.checkNicknameExist(userInfo.nickname);
+    if (checkNickname !== null) {
+      return res.status(400).json('닉네임이 이미 존재합니다.')
+    }
+    const updateUser = await query.updateUser(user._id, userInfo)
+    if (updateUser) {
+      return res.status(200).redirect('/article/lists')
+    }
+    return res.status(400).json('수정되지 않았습니다.');
+  } 
 }
