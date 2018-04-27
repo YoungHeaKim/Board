c73d8ea53d23e7eb4e811fca510e1fc733aeff13
younghea <dydgo9507@gmail.com>
2018-04-28 00:37:46 +0900

    필요없는 passport와 path설정이 잘못되어있는 부분 고침

--- src/user/user.js
+++ src/user/user.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const passport = require('passport');
 
 const cookie = require('./user.cookie.controller');
 const register = require('./user.register.controller');
@@ -16,7 +15,7 @@ router.get('/login', (req, res) => {
 
 // 회원가입부분
 router.get('/register', (req, res) => {
-  res.render(path.join(__dirname, '../../views/LogIn/register.ejs'));
+  res.render(path.join(__dirname, '../views/LogIn/register.ejs'));
 });
 
 // 로그인
@@ -27,5 +26,20 @@ router.get('/logout', cookie.cookieRemove);
 
 // 회원가입
 router.post('/register', register.singUp);
+
+// user 수정하는 페이지
+router.get('/edit', (req, res) => {
+  res.render(path.join(__dirname, '../views/login/edit.ejs'));
+})
+
+// 수정하는 부분(ejs에서)
+router.post('/edit', register.edit);
+// 수정하는 부분(back)
+router.put('/edit', register.edit);
+
+// 로그인 되었을 때 오는 부분
+router.get('/success', (req, res) => {
+  res.send(req.user);
+})
 
 module.exports = router;
