ec1f9c5e77ed82e567f5e56af62a385cb1b6e099
younghea <dydgo9507@gmail.com>
2018-05-09 19:33:58 +0900

    필요없는 console삭제

--- src/article/article.delete.controller.js
+++ src/article/article.delete.controller.js
@@ -2,8 +2,7 @@ const query = require('../Query');
 const ms = require('../message');
 
 exports.delete = async (req, res) => {
-  const article = await query.removeArticle({ _id: req.params._id })
-  console.log(article);
+  const article = await query.removeArticle({ _id: req.params._id });
   if (!article) {
     return res.status(500).json({ error: "데이터베이스 연결상에 문제가 있습니다." });
   }
