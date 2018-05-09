87ff4336f876999772e91714d33fad5c96ba1564
younghea <dydgo9507@gmail.com>
2018-05-09 19:35:46 +0900

    필요없는부분 삭제 및 오류나는 부분 수정

--- src/article/article.js
+++ src/article/article.js
@@ -3,12 +3,8 @@ const multer = require('multer');
 const uuid = require('uuid');
 const path = require('path');
 
-// csurf 셋팅
-const csrf = require('csurf');
-const csrfProtection = csrf({ cookie: true });
-
 //이미지 저장되는 위치 설정
-const uploadDir = path.join(__dirname, '../src/uploads');
+const uploadDir = path.join(__dirname, '../uploads');
 const fs = require('fs');
 
 // multer 셋팅
@@ -26,36 +22,37 @@ const getting = require('./article.get.controller');
 const posting = require('./article.post.controller');
 const putting = require('./article.put.controller');
 const deleting = require('./article.delete.controller');
-const loginRequired = require('../libs/loginRequired');
 
 const app = express();
 
 const router = express.Router();
 
+// 게시글 보여주는 페이지
+router.get('/lists/:_id', getting.articlePage);
+
 // 메인페이지
 router.get('/lists', getting.mainPage);
 
-// 게시글 보여주는 페이지
-router.get('/lists/:_id', loginRequired, getting.articlePage);
+// 게시글 수정하는 페이지
+router.get('/edit/:_id', getting.editPage);
 
 // 게시글 등록하는 페이지
-router.get('/new', loginRequired, (req, res) => {
-  res.render('admin/form', { article : "", csrfToken: req.csrfToken() });
+router.get('/new', (req, res) => {
+  res.render('admin/form', { article : "" });
 })
 
-// 게시글 수정하는 페이지
-router.get('/edit/:_id', loginRequired, csrfProtection, getting.editPage);
-
-// 게시글 삭제하는 부분
-router.delete('/lists/:_id', loginRequired, deleting.delete);
-
 // 게시글 등록하는 부분
-router.post('/new', loginRequired, upload.single('thumbnail'), csrfProtection, posting.createArticle);
+router.post('/new', upload.single('thumbnail'), posting.createArticle);
 
 // 게시글 수정하는 부분
-router.put('/edit/:_id', loginRequired, upload.single('thumbnail'), csrfProtection, putting.edit);
+router.post('/edit/:_id', upload.single('thumbnail'), putting.edit);
+router.put('/edit/:_id', upload.single('thumbnail'), putting.edit);
+
+// 게시글 삭제하는 부분
+router.get('/delete/:_id', deleting.delete);
+router.delete('/delete/:_id', deleting.delete);
 
 // summernote 부분
-router.post('/ajax_summernote', loginRequired, upload.single('thumbnail'), posting.summernote);
+router.post('/ajax_summernote', upload.single('thumbnail'), posting.summernote);
 
 module.exports = router;
