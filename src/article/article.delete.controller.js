7d30d55bc29b61e2e23ec488161f46f587dcf52c
younghea <dydgo9507@gmail.com>
2018-05-12 17:07:20 +0900

    삭제부분에서 오류가 나던 삭제시에 s3에 있는 파일이 삭제가 되지 않는 부분을 수정하였다.

--- src/article/article.delete.controller.js
+++ src/article/article.delete.controller.js
@@ -1,7 +1,24 @@
+require('dotenv').config();
+
 const query = require('../Query');
 const ms = require('../message');
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
 
 exports.delete = async (req, res) => {
+  const articleRemove = await query.findArticleById(req.params._id);
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: articleRemove.thumbnail
+  }
+  s3.deleteObject(params, (err, data, next) => {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log(data)
+    }
+  })
+  
   const article = await query.removeArticle({ _id: req.params._id });
   if (!article) {
     return res.status(500).json({ error: "데이터베이스 연결상에 문제가 있습니다." });
