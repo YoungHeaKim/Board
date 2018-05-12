75cef865b82a9bbd3a5036c9705c45254d1cc14b
younghea <dydgo9507@gmail.com>
2018-05-12 17:16:58 +0900

    수정부분 완료

--- src/article/article.put.controller.js
+++ src/article/article.put.controller.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const query = require('../Query');
 const ms = require('../message');
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
 
 exports.edit = async (req, res) => {
   // 1. 수정을 원하는 게시글을 찾아온다.
@@ -7,22 +11,38 @@ exports.edit = async (req, res) => {
   if (!article) {
     return res.status(400).json('찾으시는 글이 없습니다.')
   }
-  // 2. 요청중에 파일이 존재 할 때 이전이미지를 지운다.
-  if (req.file) {
-    consol.log('요청파일 존재 시 이전이미지 삭제부분')
-    fs.unlinkSync(uploadDir + '/' + article.thumbnail);
+  // 2. 만약 기존파일과 새로운 파일이 다르면 기존파일은 삭제 해준다.
+  if(req.thumbnail !== req.file.key) {
+    console.log('요청파일 존재 시 이전이미지 삭제부분')
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: article.thumbnail
+    }
+    s3.deleteObject(params, (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+    })
   }
+  // 3. 파일 수정
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: article.thumbnail
+  }
+  s3.putObject(params, (err, data) => {
+    if (err) {
+      console.log(err)
+    }
+  })
 
   // 3. 찾아온 게시글을 수정하고 그 수정한 데이터를 articleEdit에 저장
   const articleEdit = {
     title: req.body.title,
-    thumbnail: (req.file) ? req.file.filename : article.thumbnail,
+    thumbnail: (req.file) ? req.file.key : article.thumbnail,
     content: req.body.content
   }
-  console.log(articleEdit);
   // 4. 저장한 articleEdit을 데이터베이스에 저장
   const updateArticle = await query.updateTitleAndDescription(article._id, articleEdit);
-
   if (updateArticle) {
     return res.status(200).json('성공')
   }
